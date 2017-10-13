11f482545d9bb6093e7552dc6156ccda1e6df843
Alan <aendersoy@gmail.com>
2017-10-14 01:20:35 +0300

    Comments

--- main.go
+++ main.go
@@ -16,33 +16,47 @@ import (
 )
 
 func main() {
+	// Initialize gin router
 	router := gin.Default()
+
+	// Setting public folder as main static file base
 	router.LoadHTMLGlob("public/*")
+
+	// Router group that holds all API endpoints
 	api := router.Group("/")
 	{
 		api.GET("/", index)
 		api.GET("/api", scrapePage)
 	}
+
+	// Port definition
 	router.Run(":8081")
 }
 
+// Index router to render HTML file
 func index(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.html", "")
 }
 
+// Function to execute the scrape API
 func scrapePage(c *gin.Context) {
+
+	// Get 'url' as query:
 	url := c.Query("url")
 
+	// GET html content from 'url'
 	resp, err := http.Get(url)
 	if err != nil {
 		panic(err)
 	}
 
+	// Parse the html content and return as 'root'
 	root, err := html.Parse(resp.Body)
 	if err != nil {
 		panic(err)
 	}
 
+	// Select tags from 'root' and return as 'title'
 	title, ok := scrape.Find(root, scrape.ByTag(atom.Title))
 	if ok {
 		c.JSON(http.StatusOK, gin.H{"title": scrape.Text(title)})
